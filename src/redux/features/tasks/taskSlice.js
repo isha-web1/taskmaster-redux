a8fd66aa91b72a995e553ffb0c658c374ec9e8bc
isha-web1 <isha.web1@gmail.com>
2023-10-09 15:35:55 +0600

    add task to redux

--- src/redux/features/tasks/taskSlice.js
+++ src/redux/features/tasks/taskSlice.js
@@ -7,7 +7,17 @@ const initialState = {
 const taskSlice = createSlice({
     name : 'taskSlice',
     initialState,
-    reducers : {},
-});
+    reducers : {
+        addTask : (state, {payload})=>{
+            if(state.task.length === 0){
 
+                state.task.push({id: 1, ...payload})
+            } else{
+                const lastElement = state.task.at(-1);
+                state.task.push({id: lastElement.id + 1, ...payload})
+            }
+        }
+    },
+});
+export const {addTask} = taskSlice.actions;
 export default taskSlice.reducer;
